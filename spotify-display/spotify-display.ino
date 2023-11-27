a375f9a3d3de3d1f8d40137c05db1a299128aaca
sam <skel4720@uni.sydney.edu.au>
2023-11-27 14:45:12 +1100

    restarted, initialised basic test for the screen

--- spotify-display/spotify-display.ino
+++ spotify-display/spotify-display.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include "DFRobot_GDL.h"
+
+#define POT A0
+#define TFT_CS  D6
+#define TFT_RST D5
+#define TFT_DC  D4
+
+DFRobot_ST7789_240x320_HW_SPI screen(/*dc=*/TFT_DC,/*cs=*/TFT_CS,/*rst=*/TFT_RST);
+
+void setup() {
+  Serial.begin(115200);
+  screen.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+    testDrawPixel();
+    testLine();
+    testFastLines(COLOR_RGB565_PURPLE,COLOR_RGB565_YELLOW);       
+    testRects(COLOR_RGB565_BLACK,COLOR_RGB565_WHITE);
+    testRoundRects();
+    testCircles(24,COLOR_RGB565_BLUE);
+    testTriangles(COLOR_RGB565_YELLOW);
+    testPrint();
 
 }
