4d42a682f90d8015bbbd3a9724b2e9a20df3b41c
sam <skel4720@uni.sydney.edu.au>
2023-11-28 13:17:43 +1100

    animated playback bar has new animations for opening and closing on pause/play, more efficient with cached waveform and more robust cleaning methods

--- spotify-display/spotify-display.ino
+++ spotify-display/spotify-display.ino
@@ -12,56 +12,106 @@
 DFRobot_ST7789_240x320_HW_SPI screen(TFT_DC, TFT_CS, TFT_RST);
 
 class PlaybackBar {
-  public:
-    int trackLength = 100;
-    int progress = 70;
-    bool playing = -1;
+  private:
+    bool playing = 0;
+    bool playStateFlag = 0;
     uint16_t time = 0;
     uint16_t color;
     int x;
     int y;
     int width;
     int height;
-    float period = 4.0f;
-    float amplitude = height;
-    std::vector<int> wave;
+    int amplitude;
+    int* wave;
+    int* closingWave;
+    int numSamples;
+    int prevProgressX = -1;
+    int deltaT = 2;
 
+  public:
+    int progress = 70;
+    int trackLength = 100;
 
-    PlaybackBar(int x, int y, int width, int height, uint16_t color) {
+    PlaybackBar(int x, int y, int width, int height, int amplitude, int numSamples, uint16_t color) {
       this->x = x;
       this->y = y;
       this->width = width;
       this->height = height;
       this->color = color;
+      this->amplitude = amplitude;
+      this->numSamples = numSamples;
+      this->wave = (int*)malloc(sizeof(int) * numSamples);
+      this->closingWave = (int*)malloc(sizeof(int) * numSamples);
 
-      for (int i = 0; i < 2*period; i++) {
-        wave.push_back(sin(i/period) * amplitude);
+      int idx = 0;
+      for (float i = 0; i < 2 * PI; i += 2*PI / (float)numSamples) {
+        this->wave[idx] = sin(i) * amplitude;
+        this->closingWave[idx] = this->wave[idx++]/2;
       }
     }
 
     void draw() {
+      if (!playing && !playStateFlag) {
+        return;
+      }
 
       int bound = x + width * (progress/(float)trackLength);
-      screen.drawFastHLine(bound, y, width-x, COLOR_RGB565_WHITE);
-      screen.drawFastVLine(bound, y-2*height, 4*height, COLOR_RGB565_WHITE);
-      screen.drawFastVLine(bound-1, y-2*height, 4*height, COLOR_RGB565_BLACK);
-      screen.drawFastVLine(bound-2, y-2*height, 4*height, COLOR_RGB565_BLACK);
-      screen.drawFastVLine(bound-3, y-2*height, 4*height, COLOR_RGB565_BLACK);
+      // Stop playing
+      if (!playStateFlag && playing) {
 
-      for (int i = x; i < bound; i++) {
-        double scaledTime = time/4;
-        double scaledTimePrev = (time-1)/4;
+        // Animate wave closing back to straight line when music is paused
+        for (int i = x; i < bound; i++) {
+          screen.drawPixel(i, y + wave[(time + i - deltaT)%numSamples], COLOR_RGB565_BLACK);
+          screen.drawPixel(i, y + closingWave[(time + i - deltaT)%numSamples], color);
+        }
+        delay(30);
+        for (int i = x; i < bound; i++) {
+          screen.drawPixel(i, y + closingWave[(time + i - deltaT)%numSamples], COLOR_RGB565_BLACK);
+          screen.drawPixel(i, y, color);
+        }
+        screen.drawFastHLine(x, y, width, color);
+        screen.drawFastVLine(bound, y-2*height, 4*height, color);
+        playing = 0;
+        return;
 
-        Serial.println((time + i)%8);
-        Serial.println(wave[(time + i)%8]);
-        screen.drawPixel(i, y + wave[(time + i)%8], COLOR_RGB565_BLACK);
-        screen.drawPixel(i, y + wave[(time + i)%8], color);
+      // Start playing after pause
+      } else if (playStateFlag && !playing) {
+        // Animate wave opening to full height when music starts playing again
+        for (int i = x; i < bound; i++) {
+          screen.drawPixel(i, y, COLOR_RGB565_BLACK);
+          screen.drawPixel(i, y + closingWave[(time + i)%numSamples], color);
+        }
+        delay(30);
+        for (int i = x; i < bound; i++) {
+          screen.drawPixel(i, y + closingWave[(time + i)%numSamples], COLOR_RGB565_BLACK);
+          screen.drawPixel(i, y + wave[(time + i)%numSamples], color);
+        }
+        playing = 1;
       }
-      time++;
+
+      // Clear pixels between previous progress bar and current
+      for (int i = prevProgressX; i < bound; i++) {
+        screen.drawFastVLine(i, y-2*height, 4*height, COLOR_RGB565_BLACK);
+      }
+      screen.drawFastHLine(bound, y, width-x, color);
+      screen.drawFastVLine(bound, y-2*height, 4*height, color);
+      prevProgressX = bound;
+
+      // Draw wave
+      for (int i = x; i < bound; i++) {
+        screen.drawPixel(i, y + wave[(time + i - deltaT)%numSamples], COLOR_RGB565_BLACK);
+        screen.drawPixel(i, y + wave[(time + i)%numSamples], color);
+      }
+      time+=deltaT;
+    }
+
+    void setPlayState(bool state) {
+      playStateFlag = state;
     }
 };
 
-PlaybackBar playbackBar(15, 100, TFT_WIDTH-15, 5, COLOR_RGB565_WHITE);
+PlaybackBar playbackBar(15, 280, TFT_WIDTH-30, 5, 5, 40, COLOR_RGB565_WHITE);
+int play = 0;
 void setup() {
   Serial.begin(115200);
   screen.begin();
@@ -71,9 +121,14 @@ void setup() {
 void loop(){
   delay(50);
   playbackBar.draw();
-  playbackBar.progress++;
-  if (playbackBar.progress >= playbackBar.trackLength) {
-    playbackBar.progress = 0;
-    screen.fillScreen(COLOR_RGB565_BLACK);
+  if (play++%50 < 30) {
+    playbackBar.setPlayState(1);
+    playbackBar.progress++;
+    if (playbackBar.progress >= playbackBar.trackLength) {
+      playbackBar.progress = 0;
+      screen.fillScreen(COLOR_RGB565_BLACK);
+    }
+  } else {
+    playbackBar.setPlayState(0);
   }
 }
